aacab75005192c0cc31dc513e6c1486f28dea296
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-04 21:54:14 -0600

    refactored some comments

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", () => {
     document.body.addEventListener("keypress", () => {
-        //a ha so that's why there's such a thing as a "non breaking space" because just adding a space after hello causes it to line break. Gotcha. 
+        //a ha so that's why there's such a thing as a "non breaking space" 
+        //because just adding a space after hello causes it to line break. Gotcha. 
         document.getElementById('radioLog').append("hello ")
     })
 })
