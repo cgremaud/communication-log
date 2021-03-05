94b2a3034998ec66aa85112eb32471e8a44ce29a
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-04 21:53:16 -0600

    added comments to explain wtf I did.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", () => {
     document.body.addEventListener("keypress", () => {
+        //a ha so that's why there's such a thing as a "non breaking space" because just adding a space after hello causes it to line break. Gotcha. 
         document.getElementById('radioLog').append("hello ")
     })
 })
